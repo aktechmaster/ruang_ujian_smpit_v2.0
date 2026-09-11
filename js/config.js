33b2a4733bee5c45b843dd68855d533bfac9778f
AKTM <andrea.yk44@guru.smp.belajar.id>
2026-09-11 14:18:57 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    GAS_URL: "https://script.google.com/macros/s/AKfycbxF3g7LSwr61mNuVqOApDXnmjZsY5putWlumzL_GnJJN8hOPqhTk0MR9aPRnmbFJX1x/exec",
+    SPREADSHEET_API_URL: "https://script.google.com/macros/s/AKfycbxF3g7LSwr61mNuVqOApDXnmjZsY5putWlumzL_GnJJN8hOPqhTk0MR9aPRnmbFJX1x/exec",
+    SUBMIT_API_URL: "https://script.google.com/macros/s/AKfycbym_6liYh9AXBoHuDr_kuvuJzyxb1JNfYQezHGTlUOQVmJxU23qzkhVLOt7o3WCT1tM/exec",
+    DURASI_MAPEL: {
+        "PKN": 90,
+        "MTK": 120,
+        "IPA": 90,
+        "IPS": 90,
+        "B_INDO": 120,
+        "B_INGG": 120,
+        "PAI": 90
+    },
+    PROCTOR: {
+        ENABLE_ANTI_TAB_SWITCH: true,
+        MAX_TAB_SWITCH_WARNINGS: 3,
+        ENABLE_DISABLE_RIGHT_CLICK: true,
+        ENABLE_DISABLE_DEVTOOLS_KEYS: true
+    }
+};
